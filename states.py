7fce7d66f9bfe0f2e621ed1d9a33451f0a2c8646
MaGaroo <amoomajid99@gmail.com>
2018-07-16 12:41:20 +0430

    add basic states

---
+++ states.py
@@ -0,0 +1,30 @@
+states = {
+
+    "/": None,
+    "/login/": None,
+    "/register/": None,
+    "/edit_profile/": None,
+    "/edit_password/": None,
+    "/logout/": None,
+
+    "/student/": None,
+    "/student/courses/": None,
+    "/student/workbook/": None,
+    "/student/choose_course/": None,
+    "/student/assessment/": None,
+
+    "/teacher/": None,
+    "/teacher/courses/": None,
+    "/teacher/students/": None,
+    "/teacher/course_request/": None,
+    "/teacher/scores/": None,
+
+    "/manager/": None,
+    "/manager/teachers/": None,
+    "/manager/students/": None,
+    "/manager/courses/": None,
+    "/manager/course_requests/": None,
+    "/manager/new_users/": None,
+    "/manager/best_students/": None,
+
+}
