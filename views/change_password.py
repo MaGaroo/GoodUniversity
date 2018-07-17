13537e50eeda5d7b4f99e353e0ddb3e3254e34ac
MaGaroo <amoomajid99@gmail.com>
2018-07-18 00:14:19 +0430

    add change password view

---
+++ views/change_password.py
@@ -0,0 +1,17 @@
+from utils import show_messages, get_secret_input
+
+
+class ChangePasswordView(object):
+    def run(self, site, messages=None):
+        site.clear()
+        show_messages(messages)
+        previous_password = get_secret_input('Previous Password: ')
+        if previous_password != site.active_user.password:
+            return ['You entered your previous password incorrectly.']
+        new_password = get_secret_input('New Password: ')
+        verify_password = get_secret_input('Verify it: ')
+        if new_password != verify_password:
+            return ['The passwords you entered do not match.']
+        site.active_user.password = new_password
+        site.state = '/{}/'.format(site.active_user.role.lower())
+        return None
