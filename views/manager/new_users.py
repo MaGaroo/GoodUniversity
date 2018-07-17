cb2e7e45ddebd3edf0591ae5e1cc47dea62da9ab
MaGaroo <amoomajid99@gmail.com>
2018-07-18 02:12:46 +0430

    add new users view

---
+++ views/manager/new_users.py
@@ -0,0 +1,38 @@
+from utils import show_messages, paginator, choose_from_menu, get_input, pprint_table
+
+
+class ChooseCourseView(object):
+
+    def run(self, site, messages=None):
+        site.clear()
+        show_messages(messages)
+        user_list = paginator(site.get_unverified_users(site.active_user), 20)
+        page_number = 0
+        while True:
+            pprint_table('New Users', user_list[page_number],
+                         foot='Page {} of {}'.format(page_number, len(user_list)))
+            menu = ['Home']
+            if page_number - 1 >= 0:
+                menu.append('Previous Page')
+            if page_number + 1 < len(user_list):
+                menu.append('Next Page')
+            menu.append('Accept')
+            menu.append('Reject')
+            choice = choose_from_menu(menu)
+            if choice == 'Home':
+                site.state = '/manager/'
+                break
+            elif choice == 'Previous Page':
+                page_number -= 1
+            elif choice == 'Next Page':
+                page_number += 1
+            else:
+                student_row = get_input("Enter row number of the student: ")
+                if student_row >= len(user_list[page_number]) or student_row < 0:
+                    return ['Invalid row number!']
+                if choice == 'Accept':
+                    site.accept_user(user_list[page_number][student_row])
+                else:
+                    site.reject_user(user_list[page_number][student_row])
+                return ['Operation completed successfully.']
+        return None
