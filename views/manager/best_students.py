7df8bd7347133682a66f10a4214ccd555ace1b3b
MaGaroo <amoomajid99@gmail.com>
2018-07-18 01:48:26 +0430

    add best students view

---
+++ views/manager/best_students.py
@@ -0,0 +1,31 @@
+from utils import show_messages, paginator, choose_from_menu, pprint_table
+
+
+class BestStudentsView(object):
+
+    def run(self, site, messages=None):
+        site.clear()
+        show_messages(messages)
+        student_list = [(student.average_score(), student) for student in site.student_list]
+        student_list.sort()
+        student_list = [[t[0], t[1].name, t[1].serial, t[1].field] for t in student_list]
+        student_list = paginator(student_list, 20)
+        page_number = 0
+        while True:
+            pprint_table('Students', site.student_list,
+                         foot='Page {} of {}'.format(page_number, len(student_list)),
+                         cols_title=['Avg. Score', 'Name', 'Student No.', 'Field'])
+            menu = ['Home']
+            if page_number - 1 >= 0:
+                menu.append('Previous Page')
+            if page_number + 1 < len(student_list):
+                menu.append('Next Page')
+            menu.append('Select')
+            choice = choose_from_menu(menu)
+            if choice == 'Home':
+                site.state = '/manager/'
+                break
+            elif choice == 'Previous Page':
+                page_number -= 1
+            elif choice == 'Next Page':
+                page_number += 1
