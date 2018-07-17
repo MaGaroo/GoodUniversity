c38b2d6745edf6de0f63e066e9aa550508a4a8f1
MaGaroo <amoomajid99@gmail.com>
2018-07-18 01:31:04 +0430

    add teacher views to teacher init file

--- views/teacher/__init__.py
+++ views/teacher/__init__.py
@@ -1,1 +1,5 @@
+from .add_score import AddScoreView
+from .course_list import TeacherCourseListView
+from .course_request import CourseRequestView
+from .students_list import StudentsListView
 from .teacher import TeacherView
