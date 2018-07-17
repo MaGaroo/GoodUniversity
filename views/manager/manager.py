32414180aada31988a565864a735f6a6e7336574
MaGaroo <amoomajid99@gmail.com>
2018-07-18 01:59:21 +0430

    change a menu choice title

--- views/manager/manager.py
+++ views/manager/manager.py
@@ -7,7 +7,7 @@ class TeacherView(object):
         'Students': '/manager/students/',
         'Courses': '/manager/courses/',
         'Course Requests': '/manager/course_requests/',
-        'Unverified Students': '/manager/new_users/',
+        'New Users': '/manager/new_users/',
         'Best Students': '/manager/best_students/',
         'Edit Profile': '/manager/edit_profile/',
         'Change Password': '/change_password/',
